dab72533476db150748e54f64c8c0728c9e491a9
kdelay <kdelay20@gmail.com>
2024-05-31 19:28:10 +0900

    init

---
+++ src/main/java/company/commute/CommuteApplication.java
@@ -0,0 +1,12 @@
+package company.commute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteApplication.class, args);
+	}
+
+}
